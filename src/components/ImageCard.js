208e09890e4b71a0416d4360003e6004981d901d
hassaan <hassaan.habib@empglabs.com>
2021-08-26 17:00:22 +0500

    Added next image and image loader

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Home.module.css";
 import LazyLoad from "react-lazyload";
 import router from "next/router";
 import { saveImageData } from "../actions/UnsplashImageDetailAction";
 import { useDispatch, useSelector } from "react-redux";
+import Image from "next/image";
+
+import ContentLoader from "react-content-loader";
+
+const myLoader = ({ src, width, quality }) => {
+  return `https://unsplash.com/photos/hkWB8PN_8MU/download`;
+};
 
 function ImageCard(props) {
   const dispatch = useDispatch();
@@ -12,9 +19,20 @@ function ImageCard(props) {
   const { title } = props;
 
   return (
-    <div style={{ height: 400, width: 500, margin: "15px 0px 25px 0" }}>
+    <div
+      style={{
+        height: 400,
+        width: 500,
+        margin: "15px 0px 25px 0",
+        cursor: "pointer",
+      }}
+      className={styles.card}
+    >
       <LazyLoad height={400}>
-        <img
+        <Image
+          loader={() => src}
+          blurDataURL={"07-loader.gif"}
+          placeholder="blur"
           src={src}
           title={title}
           alt={title}
@@ -22,9 +40,8 @@ function ImageCard(props) {
             saveImageData(props, dispatch);
             router.push(`unsplash/imageDetail?imageId=${id}`);
           }}
-          height={400}
-          width="auto"
-          className={styles.card}
+          height="350%"
+          width={450}
         />
       </LazyLoad>
     </div>
